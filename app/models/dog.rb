56c5481b89666a4ee45e74380f9d483e92d9fb3f
Kian Edalat <akedalat@gmail.com>
2019-02-18 14:03:27 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+attr_accessor :name
+attr_accessor :age
+attr_reader :breed
+
+@@all = []
+
+  def initialize(name, breed, age)
+    @name =name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
